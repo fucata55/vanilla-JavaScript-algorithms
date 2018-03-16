010872a5562d8654a48877c702b6e55b7fa4f2da
fucata <sliong325@gmail.com>
2018-03-15 22:28:50 -0500

    Finished Adjacent Elements Product algorithm

---
+++ adjacent-elements-product.js
@@ -0,0 +1,32 @@
+//Input/Output
+//
+//[execution time limit] 4 seconds (js)
+//
+//[input] array.integer inputArray
+//An array of integers containing at least two elements.
+//Guaranteed constraints:
+//2 ≤ inputArray.length ≤ 10,
+//-1000 ≤ inputArray[i] ≤ 1000.
+//
+//[output] integer
+//The largest product of adjacent elements.
+//
+//
+//My solution:
+let adjacentElementsProduct = inputArray => {
+    let highestProduct = -1001;
+    for (let i=1; i<inputArray.length; i+=2) {
+        if(inputArray[i+1]) {
+            if (inputArray[i-1]*inputArray[i]>=inputArray[i+1]*inputArray[i] && inputArray[i-1]*inputArray[i]>highestProduct) {
+                highestProduct=inputArray[i-1]*inputArray[i];
+            }
+            else if (inputArray[i-1]*inputArray[i]<=inputArray[i+1]*inputArray[i] && inputArray[i+1]*inputArray[i]>highestProduct) {
+                highestProduct=inputArray[i+1]*inputArray[i];
+            }
+        }
+        else if(!inputArray[i+1] && inputArray[i-1]*inputArray[i]>highestProduct) {
+            highestProduct=inputArray[i-1]*inputArray[i];
+        }
+    }
+    return highestProduct
+}
